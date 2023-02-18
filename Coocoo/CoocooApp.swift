f2f5fdaeca19f1df6705f33b3397fee16cb4d4f5
vzooot <vzotikas@hotmail.com>
2023-02-18 11:14:00 +0100

    Initial Commit

---
+++ Coocoo/CoocooApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoocooApp.swift
+//  Coocoo
+//
+//  Created by Administrator on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct CoocooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
